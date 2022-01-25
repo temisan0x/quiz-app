839d4bb9f017e7d66b749c567970101088892475
Heytemisan <temyicy@gmail.com>
2022-01-25 11:05:35 +0100

    feat: added QuestionCard props to App.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
-const QuestionCard = () => {
+type Props = {
+    question:string;
+    answers:string[];
+    callback:any;
+    userAnswer:any;
+    questionNum:number;
+    totalQuestions:number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+    question, 
+    answers, 
+    callback,
+    userAnswer,
+    questionNum,
+    totalQuestions,
+    }) => {
     return (
         <div>
-            Question Card
+            <p className="number">
+                Question: {questionNum} / {totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{__html: question}}/>
+            <div>
+                {answers.map(answer => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{__html:answer}}/>
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
